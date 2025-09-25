df650c244b261b05e23702ac1402a1fd8d9c2e68
amiltonbandeira <amiltonbraulioadaobandeira@gmail.com>
2025-09-25 21:42:14 +0100

    [NEW] hello_world.c file again

---
+++ hello_world/hello_world.c
@@ -0,0 +1,5 @@
+#include <stdio.h> // This adds or includes the standard input/output library informations in the module
+int main() { //This creates the/a new function called main with no arguments values
+		printf("Hello World!\n");//This calls the printf function to display a message
+}//This closes the program
+
